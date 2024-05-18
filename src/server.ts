36e7d90473280a9a20bf9c5bbe459c9b90a4bf42
astrit11 <astritmaliku@yahoo.com>
2024-05-18 16:55:06 +0200

    Updated db initialization and starting the app from callback function to self invoking function

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,24 @@
-import express from 'express';
-import { sequelize } from './config/database';
-import productRoutes from './routes/productRoutes';
-import variantRoutes from './routes/variantRoutes';
+import express from "express";
+import { sequelize } from "./config/database";
+import productRoutes from "./routes/productRoutes";
+import variantRoutes from "./routes/variantRoutes";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use('/api/v1', productRoutes);
-app.use('/api/v1', variantRoutes);
+app.use("/api/v1", productRoutes);
+app.use("/api/v1", variantRoutes);
 
-// Sync database and start server
-sequelize.sync({ force: true })
-  .then(() => {
-    console.log('Database synchronized');
+(async () => {
+  try {
+    await sequelize.sync({force:false});
+    console.log("Database synchronized");
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
-  })
-  .catch(err => {
-    console.error('Unable to synchronize the database:', err);
-  });
+  } catch (error) {
+    console.error("Unable to synchronize the database:", error);
+    process.exit(1);
+  }
+})();
